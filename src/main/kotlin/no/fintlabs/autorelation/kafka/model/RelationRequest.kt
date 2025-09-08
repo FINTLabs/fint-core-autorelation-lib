8bdf824a1c0505087ae9afa81b8592c66545eeb7
Hknots <hknots99@gmail.com>
2025-09-08 19:38:35 +0200

    feat: add static function to create RelationRequest

--- src/main/kotlin/no/fintlabs/autorelation/kafka/model/RelationRequest.kt
+++ src/main/kotlin/no/fintlabs/autorelation/kafka/model/RelationRequest.kt
@@ -5,4 +5,25 @@ data class RelationRequest(
     val orgId: String,
     val resource: Any,
     val operation: RelationOperation
-)
+) {
+    companion object {
+        fun from(
+            operation: RelationOperation,
+            orgId: String,
+            domain: String,
+            pkg: String,
+            resourceName: String,
+            resource: Any
+        ) =
+            RelationRequest(
+                operation = operation,
+                type = ResourceType(
+                    domain = domain,
+                    pkg = pkg,
+                    resource = resourceName
+                ),
+                orgId = orgId,
+                resource = resource
+            )
+    }
+}
