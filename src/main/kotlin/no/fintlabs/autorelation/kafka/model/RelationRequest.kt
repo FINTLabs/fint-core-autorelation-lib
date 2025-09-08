8724e5e863696bdf81fffa026fe7eb73384f987b
Hknots <hknots99@gmail.com>
2025-09-08 19:12:53 +0200

    feat: add orgId field

--- src/main/kotlin/no/fintlabs/autorelation/kafka/model/RelationRequest.kt
+++ src/main/kotlin/no/fintlabs/autorelation/kafka/model/RelationRequest.kt
@@ -1,7 +1,8 @@
 package no.fintlabs.autorelation.kafka.model
 
 data class RelationRequest(
-    val resource: Any,
     val type: ResourceType,
+    val orgId: String,
+    val resource: Any,
     val operation: RelationOperation
 )
