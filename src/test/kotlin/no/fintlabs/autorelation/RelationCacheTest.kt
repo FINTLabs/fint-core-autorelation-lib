b68a3edfeb1d0b5b414c7f999262251d4438dce2
Hknots <hknots99@gmail.com>
2025-09-10 13:09:43 +0200

    test: add RelationCache integration test

---
+++ src/test/kotlin/no/fintlabs/autorelation/RelationCacheTest.kt
@@ -0,0 +1,41 @@
+package no.fintlabs.autorelation
+
+import no.fintlabs.autorelation.cache.RelationCache
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertNotNull
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RelationCacheTest @Autowired constructor(
+    private val relationCache: RelationCache
+) {
+
+    private val fintResourceRelations = createFintResourceRelations()
+
+    @Test
+    fun `verify FINT models in v3_19 exist within the cache`() {
+        val resourceRelations = relationCache.getResourceRelations("utdanning", "vurdering")
+        fintResourceRelations.forEach { (fintResource, fintRelationNames) ->
+            val relationNames = resourceRelations[fintResource]
+            assertNotNull(relationNames!!)
+            assertTrue(fintRelationNames.all { relationNames.contains(it) })
+        }
+    }
+
+    private fun createFintResourceRelations() = mapOf(
+        "elevvurdering" to listOf(
+            "eksamensvurdering",
+            "halvarsfagvurdering",
+            "halvarsordensvurdering",
+            "sluttfagvurdering",
+            "sluttordensvurdering",
+            "underveisfagvurdering",
+            "underveisordensvurdering"
+        ),
+        "eksamensgruppe" to listOf("gruppemedlemskap", "sensor"),
+        "elevfravar" to listOf("fravarsregistrering")
+    )
+
+}
