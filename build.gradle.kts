87f58f9c88e4481402347c38beb1b107609f1686
Hknots <hknots99@gmail.com>
2025-09-15 10:58:36 +0200

    build: setup library requirements

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,26 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.5.5"
+    id("org.springframework.boot") version "3.5.5" apply false
     id("io.spring.dependency-management") version "1.1.7"
+    id("maven-publish")
+    id("java-library")
 }
 
 group = "no.fintlabs"
-version = "0.0.1-SNAPSHOT"
+version = project.findProperty("version") ?: "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
+    }
+    withSourcesJar()
+    withJavadocJar()
+}
+
+dependencyManagement {
+    imports {
+        mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
     }
 }
 
@@ -20,6 +30,17 @@ repositories {
 }
 
 dependencies {
+    implementation("no.fintlabs:fint-model-core:0.5.0")
+    implementation("no.fintlabs:fint-model-resource:0.5.0")
+    implementation("no.fintlabs:fint-core-consumer-metamodel:2.0.0-rc-4")
+
+    testImplementation("no.fint:fint-utdanning-resource-model-java:3.19.0")
+    testImplementation("no.fint:fint-administrasjon-resource-model-java:3.19.0")
+    testImplementation("no.fint:fint-personvern-resource-model-java:3.19.0")
+    testImplementation("no.fint:fint-okonomi-resource-model-java:3.19.0")
+    testImplementation("no.fint:fint-ressurs-resource-model-java:3.19.0")
+    testImplementation("no.fint:fint-arkiv-resource-model-java:3.19.0")
+
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -36,3 +57,5 @@ kotlin {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
+apply(from = "https://raw.githubusercontent.com/FINTLabs/fint-buildscripts/master/reposilite.ga.gradle")
