fad820b54faa1b91d433c130bc2869035ce9dce7
Hknots <hknots99@gmail.com>
2026-01-13 09:28:29 +0100

    test: update ApplicationTests with configuration for metamodel service mocking

--- src/test/kotlin/no/fintlabs/ApplicationTests.kt
+++ src/test/kotlin/no/fintlabs/ApplicationTests.kt
@@ -1,15 +1,24 @@
 package no.fintlabs
 
+import no.fintlabs.metamodel.MetamodelService
 import org.junit.jupiter.api.Test
+import org.springframework.boot.SpringBootConfiguration
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
-import kotlin.test.Ignore
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 
-@Ignore
 @SpringBootTest
 class ApplicationTests {
+    @SpringBootConfiguration
+    @EnableAutoConfiguration
+    @ComponentScan("no.fintlabs")
+    class TestConfig
+
+    @MockitoBean
+    lateinit var metamodelService: MetamodelService
 
     @Test
     fun contextLoads() {
     }
-
 }
